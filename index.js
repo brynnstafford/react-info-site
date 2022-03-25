2665beaff786b9ce8cc8f3cce40de283fc61ca54
Brynn Stafford <54415447+brynnstafford@users.noreply.github.com>
2022-03-25 15:31:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+/* video 3:
+ReactDOM.render(<h1>Hello!</h1>, document.getElementById("root")) //header
+ReactDOM.render(<p>Hello!</p>, document.getElementById("root")) //paragraph
+*/
 
+/* Surprise! You probably thought you could just forget
+the line of code you just learned! Nope, not on my watch!
+
+Try to write that 1-liner of React code again! This time,
+see if you can figure out how to render an <ul> with 2+ <li>s inside*/
+ReactDOM.render(
+    <ul><li>Thing 1</li><li>Thing 2</li></ul>,
+    document.getElementById("root")
+)
