50438e3dbab6b698bf56d099b51af4b7c641ba10
13212 <brynnstafford@gmail.com>
2022-03-30 16:24:14 -0400

    progress

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
+
 /* video 3: paragraph and header
 ReactDOM.render(<h1>Hello!</h1>, document.getElementById("root"))
 ReactDOM.render(<p>Hello!</p>, document.getElementById("root")) 
 */
-
-
 
 /* video 4: unordered list with two bullets inside
 Surprise! You probably thought you could just forget
@@ -17,8 +16,6 @@ ReactDOM.render(
     document.getElementById("root")
     )
 */
-
-
 
 /* video 6: sculpting/compostability (prestyled nav bar from bootstrap)
 function Navbar() {
@@ -82,15 +79,13 @@ ReactDOM.render(
 )
 */
 
-
-
 /* video 7: declarative 
 // declarative-what should be done?
 // imparative-how should it be done?
 
 // ReactDOM.render(<h1>Hello, React!</h1>, document.getElementById("root"))
 
-/* 
+
 Challenge - recreate the above line of code in vanilla JS by creating and
 appending an h1 to our div#root (without using innerHTML).
 
@@ -99,12 +94,234 @@ appending an h1 to our div#root (without using innerHTML).
 - Give it a class name of "header"
 - append it as a child of the div#root
     
-*/
+
 const h1 = document.createElement("h1")
 h1.textContent = "This is an imparative way to program"
 h1.className = "header"
 document.getElementById("root").append(h1)
+*/
+
+/* video 8: JSX
+// const page = (
+//     <div>
+//         <h1 className="header">This is JSX</h1>
+//         <p>This is a paragraph</p>
+//     </div>
+// )
+
+Challenge: 
+
+Create a navbar in JSX:
+    - Use the semantic `nav` element as the parent wrapper
+    - Have an h1 element with the brand name of your "website"
+    - Insert an unordered list for the other nav elements
+        - Inside the `ul`, have three `li`s for "Pricing",
+        "About", and "Contact"
+    - Don't worry about styling yet - it'll just be plain-looking HTML for now
+
+
+const navbar = (
+    <nav>
+        <h1>Bob's Bistro</h1>
+        <ul>
+            <li>Menu</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(navbar, document.getElementById("root"))
+*/
+
+/* video 9: correct way to setup react
+
+//import React from "react"
+//import ReactDOM from "react-dom"
+
+const navbar = (
+    <nav>
+        <h1>Bob's Bistro</h1>
+        <ul>
+            <li>Menu</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(navbar, document.getElementById("root"))
+
+*/
+
+/* video 10:
+import React from "react"
+import ReactDOM from "react-dom"
+
+
+Challenge: fix our code!
+
+Don't forget, you're not using CDNs anymore, so there's no
+global "ReactDOM" variable to use anymore.
+
+
+const page = (
+    <div>
+        <h1>My awesome website in React</h1>
+        <h3>Reasons I love React</h3>
+        <ol>
+            <li>It's composable</li>
+            <li>It's declarative</li>
+            <li>It's a hireable skill</li>
+            <li>It's actively maintained by skilled people</li>
+        </ol>
+    </div>
+)
+
+ReactDOM.render(page, document.getElementById("root"))
+// document.getElementById("root").append(JSON.stringify(page))
+*/
+
+/* video 11: (need png file to work)
+import React from "react"
+import ReactDOM from "react-dom"
+
+const page = (
+    <div>
+        <img src="./react-logo.png" width="40px" />
+        <h1>Fun facts about React</h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+    </div>
+)
+
+ReactDOM.render(page, document.getElementById("root"))
+*/
+
+/* video 12:
+1. Why do we need to `import React from "react"` in our files?
+React is what defines JSX
+
+2. If I were to console.log(page) in index.js, what would show up?
+A JavaScript object. React elements that describe what React should
+eventually add to the real DOM for us.
+
+3. What's wrong with this code:
+```
+const page = (
+    <h1>Hello</h1>
+    <p>This is my website!</p>
+)
+```
+We need our JSX to be nested under a single parent element
+
+4. What does it mean for something to be "declarative" instead of "imperative"?
+Declarative means I can tell the computer WHAT to do 
+and expect it to handle the details. Imperative means I need
+to tell it HOW to do each step.
+
+5. What does it mean for something to be "composable"?
+We have small pieces that we can put together to make something
+larger/greater than the individual pieces.
+*/
+
+/* video 13:
+
+*/
+
+
+/* video 14:
+
+*/
 
 
 
+/* video 15:
 
+*/
+
+
+
+/* video 16:
+
+*/
+
+
+
+/* video 17:
+
+*/
+
+
+
+/* video 18:
+
+*/
+
+
+
+/* video 19:
+
+*/
+
+
+/* video 20:
+
+*/
+
+
+
+/* video 21:
+
+*/
+
+
+
+/* video 22:
+
+*/
+
+
+
+/* video 23:
+
+*/
+
+
+
+/* video 24:
+
+*/
+
+
+/* video 25:
+
+*/
+
+
+
+/* video 26:
+
+*/
+
+
+
+/* video 27:
+
+*/
+
+
+/* video 28:
+
+*/
+
+
+
+/* video 29:
+
+*/
